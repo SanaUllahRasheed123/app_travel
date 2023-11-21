715a2419ea88d8411a100d6b3ee2fae7aa98a0e6
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-11-21 12:32:36 +0500

    updating

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import Hero from '@/components/Hero';
 
 
 export const metadata: Metadata = {
@@ -22,6 +23,7 @@ export default function RootLayout({
         {children}
 
         </main>
+        <Hero/>
         <Footer/>
         </body>
     </html>
