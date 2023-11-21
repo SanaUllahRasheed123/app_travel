f8d7c29122b9f59c390297a27f1e761837de759d
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-11-21 13:11:51 +0500

    updating

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Button from "./Button"
 const Hero = () => {
   return (
     <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 
@@ -34,10 +35,24 @@ const Hero = () => {
           </div>
           <p className="bold-16 lg:bold-20 text-blue-70">
             198k
-            <span className="regular-16 lg:regular-20 ml-1 underline">
-              Excellent
+            <span className="regular-16 lg:regular-20 ml-1 ">
+              Excellent Reviews
             </span>
           </p>
+        </div>
+        <div className="flex w-full gap-3 sm:flex-row">
+          <Button 
+          type="button"
+          title="Download App"
+          varient="btn_green"
+          />
+           <Button 
+          type="button"
+          title="How we work?"
+          icon="/play.svg"
+          varient="btn_white_text"
+          
+          />
         </div>
       </div>
 
